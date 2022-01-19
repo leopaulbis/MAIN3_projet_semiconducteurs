372af3a05a9d386708c964078256200425425353
leo paul <92988071+leopaulbis@users.noreply.github.com>
2022-01-19 08:52:06 +0100

    Add files via upload

--- projet_Main3/programmes_python/thinfilmsSiN100nm.py
+++ projet_Main3/programmes_python/thinfilmsSiN100nm.py
@@ -17,7 +17,7 @@ df = pd.read_excel('SiN100nm.xlsx')  # for 50 nm took data at 90 mA
 bh=5e-6 # half width heater
 tf = 100e-9 # thin film thickness
 diffusivity_Si = 0.00008  # diffusivity of substrate material
-kappa_Si = 158.9 # Si substrate from data at 90 mA
+kappa_Si = 158.9 # Si substrate from data at 60 mA
 V0 = 0.77 # rms fundamental voltage  at the heater
 R0 = 12.983 # heater resistance
 I0 = V0/R0 # rms fundamental current  at the heater
@@ -55,18 +55,17 @@ mean_kapp = data['kappa thin film'].mean()
 print(mean_kapp)
 
 # plt.figure(1)
-# plt.figure(1)
-# plt.subplot(1, 2, 1)
-# plt.plot(np.log(4*math.pi * data['frequency']), data['Real_DTac_Si'], 'r', label='Si substrate')
-# plt.plot(np.log(4*math.pi * data['frequency']), data['DTac_thin_film'], 'b', label='SiN(100 nm) / Si')
-# plt.xlabel('ln(2\u03C9)')
-# plt.ylabel('In phase temperature oscillation (K)')
-# # plt.xscale('log')
-# plt.margins(x=0)
-# plt.legend(loc='best')
+plt.subplot(1, 2, 1)
+plt.plot(np.log(4*math.pi * data['frequency']), data['Real_DTac_Si'], 'r', label='Si substrate')
+plt.plot(np.log(4*math.pi * data['frequency']), data['DTac_thin_film'], 'b', label='SiN(100 nm) / Si')
+plt.xlabel('ln(2\u03C9)')
+plt.ylabel('In phase temperature oscillation (K)')
+# plt.xscale('log')
+plt.margins(x=0)
+plt.legend(loc='best')
 
 # plt.figure(2)
-# plt.subplot(1, 2, 2)
+plt.subplot(1, 2, 2)
 plt.plot(data['frequency'], data['kappa thin film'], 'b', label='SiN 100 nm')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Thermal conductivity (W/m.K)')
